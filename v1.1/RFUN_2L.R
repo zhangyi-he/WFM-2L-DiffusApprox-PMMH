acd2c68e64d50c4ae32f0d605f9a81408ae115d8
Zhangyi He <zhangyi-he@users.noreply.github.com>
2020-04-14 14:29:07 +0100

    Update RFUN_2L.R

--- v1.1/RFUN_2L.R
+++ v1.1/RFUN_2L.R
@@ -23,7 +23,7 @@ library("compiler")
 #enableJIT(1)
 
 # call C++ functions
-sourceCpp("./Code/Code v2.1/Code v2.2/CFUN_2L.cpp")
+sourceCpp("./CFUN_2L.cpp")
 
 ################################################################################
 
