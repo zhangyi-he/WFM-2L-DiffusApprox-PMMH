56bb1444b1e236a38899abd1235e5ae19b16b409
zhangyi-he <jefferyhe@outlook.com>
2020-10-11 21:58:51 +0100

    Update TEST_2L.R

--- v1.1/TEST_2L.R
+++ v1.1/TEST_2L.R
@@ -115,13 +115,6 @@ for (i in 1:sim_num) {
   sim_frq_WFD[, i] <- cmpsimulateTLWFDS(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num, data_augmentation = FALSE)[, (lst_gen - int_gen) + 1]
 }
 
-save(sel_cof, dom_par, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num, sim_num, sim_frq_WFM, sim_frq_WFD, 
-     file = "./Output/Output v2.1/Test v1.1/TEST_2L_WFM_vs_WFD.rda")
-
-load("./Output/Output v2.1/Test v1.1/TEST_2L_WFM_vs_WFD.rda")
-
-pdf(file = "./Output/Output v2.1/Test v1.1/TEST_2L_WFM_vs_WFD.pdf", width = 20, height = 10)
-par(mfrow = c(2, 2), mar = c(5.5, 5, 5.5, 2.5), oma = c(0, 0, 3, 0), cex.main = 1.75, cex.sub = 1.5, cex.axis = 1.5, cex.lab = 1.5)
 hist(sim_frq_WFM[1, ], breaks = seq(min(sim_frq_WFM[1, ], sim_frq_WFD[1, ]), max(sim_frq_WFM[1, ], sim_frq_WFD[1, ]), length.out = 50), freq = FALSE, col = rgb(0.1, 0.1, 0.1, 0.5), 
      xlim = c(min(sim_frq_WFM[1, ], sim_frq_WFD[1, ]), max(sim_frq_WFM[1, ], sim_frq_WFD[1, ])), 
      xlab = "Haplotype frequency", main = "Haplotype A1B1")
@@ -141,8 +134,6 @@ hist(sim_frq_WFM[4, ], breaks = seq(min(sim_frq_WFM[4, ], sim_frq_WFD[4, ]), max
      xlim = c(min(sim_frq_WFM[4, ], sim_frq_WFD[4, ]), max(sim_frq_WFM[4, ], sim_frq_WFD[4, ])), 
      xlab = "Haplotype frequency", main = "Haplotype A2B2")
 hist(sim_frq_WFD[4, ], breaks = seq(min(sim_frq_WFM[4, ], sim_frq_WFD[4, ]), max(sim_frq_WFM[4, ], sim_frq_WFD[4, ]), length.out = 50), freq = FALSE, col = rgb(0.8, 0.8, 0.8, 0.5), add = TRUE)
-title(paste("Histograms of the haplotype frequencies in generation", lst_gen, "under the Wright-Fisher model and the Wright-Fisher diffusion"), outer = TRUE)
-dev.off()
 
 ################################################################################
 
@@ -275,14 +266,14 @@ smp_ale_cnt <- SimData$smp_ale_cnt
 pop_ale_frq <- SimData$pop_ale_frq
 
 save(sel_cof, dom_par, rec_rat, pop_siz, smp_gen, smp_siz, smp_hap_cnt, pop_hap_frq, smp_ale_cnt, pop_ale_frq, 
-     file = "./Output/Output v2.1/Test v1.1/TEST_2L_SimData.rda")
+     file = "./TEST_2L_SimData.rda")
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_SimData.rda")
+load("./TEST_2L_SimData.rda")
 
 k <- min(smp_gen):max(smp_gen)
 smp_ale_frq <- smp_ale_cnt %*% diag(1 / smp_siz)
 
-pdf(file = "./Output/Output v2.1/Test v1.1/TEST_2L_SimData.pdf", width = 20, height = 20)
+pdf(file = "./TEST_2L_SimData.pdf", width = 20, height = 20)
 par(mfrow = c(2, 2), oma = c(0, 0, 3, 0), mar = c(5.5, 5, 5.5, 2.5), cex.main = 2, cex.sub = 1.75, cex.axis = 1.75, cex.lab = 1.75)
 plot(k, pop_ale_frq[1, ], type = 'l', lwd = 1.5, 
      xlim = c(min(smp_gen), max(smp_gen)), ylim = c(min(smp_ale_frq[1, ], pop_ale_frq[1, ]), max(smp_ale_frq[1, ], pop_ale_frq[1, ])), 
@@ -324,7 +315,7 @@ dev.off()
 #' @param ptn_num the number of subintervals divided per generation in the Euler-Maruyama method
 #' @param pcl_num the number of particles generated in the bootstrap particle filter
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_SimData.rda")
+load("./TEST_2L_SimData.rda")
 
 set.seed(test_seed)
 
@@ -341,9 +332,9 @@ pcl_num <- 5e+04
 system.time(BPF <- cmprunBPF(sel_cof, dom_par, rec_rat, pop_siz, smp_gen, smp_siz, smp_ale_cnt, ptn_num, pcl_num))
 
 save(sel_cof, dom_par, rec_rat, pop_siz, smp_gen, smp_siz, smp_ale_cnt, ptn_num, pcl_num, BPF, 
-     file = "./Output/Output v2.1/Test v1.1/TEST_2L_BPF.rda")
+     file = "./TEST_2L_BPF.rda")
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_BPF.rda")
+load("./TEST_2L_BPF.rda")
 
 lik <- rep(1, pcl_num)
 wght <- BPF$wght
@@ -351,7 +342,7 @@ for (k in 1:length(smp_gen)) {
   lik <- lik * (cumsum(wght[, k]) / (1:pcl_num))
 }
 
-pdf(file = "./Output/Output v2.1/Test v1.1/TEST_2L_BPF_Likelihood.pdf", width = 10, height = 10)
+pdf(file = "./TEST_2L_BPF_Likelihood.pdf", width = 10, height = 10)
 par(mar = c(5.5, 5, 5.5, 2.5), cex.main = 1.75, cex.sub = 1.5, cex.axis = 1.5, cex.lab = 1.5)
 plot(1:pcl_num, log(lik), type = 'l', 
      xlab = "Number of particles", ylab = "Log likelihood", main = "Log likelihood estimated with the bootstrap particle filter")
@@ -361,7 +352,7 @@ smp_hap_frq <- smp_hap_cnt %*% diag(1 / smp_siz)
 pop_hap_frq_pre_resmp <- BPF$pop_frq_pre_resmp
 pop_hap_frq_pst_resmp <- BPF$pop_frq_pst_resmp
 
-pdf(file = "./Output/Output v2.1/Test v1.1/TEST_2L_BPF_Particle.pdf", width = 20, height = 55)
+pdf(file = "./TEST_2L_BPF_Particle.pdf", width = 20, height = 55)
 par(mfrow = c(11, 4), oma = c(0, 0, 3, 0), mar = c(5.5, 5, 5.5, 2.5), cex.main = 1.75, cex.sub = 1.5, cex.axis = 1.5, cex.lab = 1.5)
 for (k in 1:length(smp_gen)) {
   hist_pst_resmp <- hist(pop_hap_frq_pst_resmp[1, , k], breaks = seq(min(pop_hap_frq_pst_resmp[1, , k], pop_hap_frq_pre_resmp[1, , k]), max(pop_hap_frq_pst_resmp[1, , k], pop_hap_frq_pre_resmp[1, , k]), length.out = 50), plot = FALSE)
@@ -414,7 +405,7 @@ dev.off()
 #' @param pcl_num the number of particles generated in the bootstrap particle filter
 #' @param gap_num the number of particles increased or decreased in the optimal particle number search
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_SimData.rda")
+load("./TEST_2L_SimData.rda")
 
 set.seed(test_seed)
 
@@ -432,14 +423,14 @@ gap_num <- 1e+02
 system.time(OptNum <- calculateOptimalParticleNum(sel_cof, dom_par, rec_rat, pop_siz, smp_gen, smp_siz, smp_ale_cnt, ptn_num, pcl_num, gap_num))
 
 save(sel_cof, dom_par, rec_rat, pop_siz, smp_gen, smp_siz, smp_ale_cnt, ptn_num, pcl_num, gap_num, OptNum,
-     file = "./Output/Output v2.1/Test v1.1/TEST_2L_OptNum.rda")
+     file = "./TEST_2L_OptNum.rda")
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_OptNum.rda")
+load("./TEST_2L_OptNum.rda")
 
 opt_pcl_num <- OptNum$opt_pcl_num
 log_lik_sdv <- OptNum$log_lik_sdv
 
-pdf(file = "./Output/Output v2.1/Test v1.1/TEST_2L_OptNum.pdf", width = 12, height = 9)
+pdf(file = "./TEST_2L_OptNum.pdf", width = 12, height = 9)
 par(mar = c(5.5, 5, 5.5, 2.5), cex.main = 1.75, cex.sub = 1.5, cex.axis = 1.5, cex.lab = 1.5)
 plot(opt_pcl_num, log_lik_sdv, type = 'b', lwd = 2, 
      xlab = "Particle number", ylab = "Log-likelihood standard deviation", main = "Optimal particle number in the PMMH")
@@ -462,7 +453,7 @@ dev.off()
 #' @param pcl_num the number of particles generated in the bootstrap particle filter
 #' @param itn_num the number of the iterations carried out in the particle marginal Metropolis-Hastings
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_SimData.rda")
+load("./TEST_2L_SimData.rda")
 
 set.seed(test_seed)
 
@@ -480,15 +471,15 @@ itn_num <- 5e+04
 system.time(PMMH <- cmprunPMMH(sel_cof, dom_par, rec_rat, pop_siz, smp_gen, smp_siz, smp_ale_cnt, ptn_num, pcl_num, itn_num))
 
 save(sel_cof, dom_par, rec_rat, pop_siz, smp_gen, smp_siz, smp_ale_cnt, ptn_num, pcl_num, itn_num, PMMH, 
-     file = "./Output/Output v2.1/Test v1.1/TEST_2L_PMMH.rda")
+     file = "./TEST_2L_PMMH.rda")
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_PMMH.rda")
+load("./TEST_2L_PMMH.rda")
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_SimData.rda")
+load("./TEST_2L_SimData.rda")
 
 sel_cof_A_chn <- PMMH$sel_cof_A_chn
 sel_cof_B_chn <- PMMH$sel_cof_B_chn
-pdf(file = "./Output/Output v2.1/Test v1.1/TEST_2L_PMMH_Traceplot.pdf", width = 20, height = 10)
+pdf(file = "./TEST_2L_PMMH_Traceplot.pdf", width = 20, height = 10)
 par(mfrow = c(2, 1), mar = c(5.5, 5, 5.5, 2.5), cex.main = 2, cex.sub = 1.75, cex.axis = 1.75, cex.lab = 1.75)
 plot(1:itn_num, sel_cof_A_chn[1:itn_num], type = 'l', 
      xlab = "Iteration", ylab = "Selection coefficient", main = "Trace plot of the selection coefficient at locus A")
@@ -508,7 +499,7 @@ sel_cof_B_chn <- sel_cof_B_chn[(1:round(length(sel_cof_B_chn) / thn_num)) * thn_
 
 grd_num <- 1e+03
 sel_cof_pdf <- kde2d(sel_cof_A_chn, sel_cof_B_chn, n = grd_num)
-pdf(file = "./Output/Output v2.1/Test v1.1/TEST_2L_PMMH_Posterior.pdf", width = 10, height = 10)
+pdf(file = "./TEST_2L_PMMH_Posterior.pdf", width = 10, height = 10)
 par(mar = c(5.5, 5, 5.5, 2.5), cex.main = 2, cex.sub = 1.75, cex.axis = 1.75, cex.lab = 1.75)
 image(sel_cof_pdf, col = colorRampPalette(rev(brewer.pal(11, 'Spectral')))(32), 
       xlab = "Selection coefficient at locus A", ylab = "Selection coefficient at locus B", 
@@ -535,7 +526,7 @@ dev.off()
 #' @param thn_num the number of the iterations for thinning
 #' @param grd_num the number of the grids in the kernel density estimation
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_SimData.rda")
+load("./TEST_2L_SimData.rda")
 
 set.seed(test_seed)
 
@@ -556,11 +547,11 @@ grd_num <- 1e+03
 system.time(BayesianProcedure <- cmprunBayesianProcedure(sel_cof, dom_par, rec_rat, pop_siz, smp_gen, smp_siz, smp_ale_cnt, ptn_num, pcl_num, itn_num, brn_num, thn_num, grd_num))
 
 save(sel_cof, dom_par, rec_rat, pop_siz, smp_gen, smp_siz, smp_ale_cnt, ptn_num, pcl_num, itn_num, brn_num, thn_num, grd_num, BayesianProcedure, 
-     file = "./Output/Output v2.1/Test v1.1/TEST_2L_BayesProc.rda")
+     file = "./TEST_2L_BayesProc.rda")
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_BayesProc.rda")
+load("./TEST_2L_BayesProc.rda")
 
-load("./Output/Output v2.1/Test v1.1/TEST_2L_SimData.rda")
+load("./TEST_2L_SimData.rda")
 
 sel_cof_A_chn <- BayesianProcedure$sel_cof_A_chn
 sel_cof_B_chn <- BayesianProcedure$sel_cof_B_chn
@@ -576,7 +567,7 @@ sel_cof_B_mmse <- BayesianProcedure$sel_cof_B_mmse
 sel_cof_B_hpd <- BayesianProcedure$sel_cof_B_hpd
 sel_cof_A_hpd <- BayesianProcedure$sel_cof_A_hpd
 
-pdf(file = "./Output/Output v2.1/Test v1.1/TEST_2L_BayesProc_Posterior.pdf", width = 20, height = 10)
+pdf(file = "./TEST_2L_BayesProc_Posterior.pdf", width = 20, height = 10)
 par(mar = c(5.5, 5, 5.5, 2.5), cex.main = 2, cex.sub = 1.75, cex.axis = 1.75, cex.lab = 1.75)
 layout(matrix(c(1, 1, 2, 3), nrow = 2, ncol = 2))
 image(sel_cof_pdf, col = colorRampPalette(rev(brewer.pal(11, 'Spectral')))(32), 
