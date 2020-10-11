672cd55d872ac508af0f4a0df356d3b6b2dbe06d
zhangyi-he <jefferyhe@outlook.com>
2020-10-11 21:49:41 +0100

    Update CFUN_2L.cpp

--- v1.0/CFUN_2L.cpp
+++ v1.0/CFUN_2L.cpp
@@ -2,7 +2,7 @@
 // Zhangyi He, Xiaoyang Dai, Mark Beaumont and Feng Yu
 
 // version 1.0
-// Two-loucs population dynamics (N/A is not allowed)
+// Two-loucs case (N/A is not allowed)
 
 // C functions
 
