0e23ae3a331c1d1d2156f3146d3ef00287011784
zhangyi-he <jefferyhe@outlook.com>
2022-07-19 22:50:55 +0100

    Update CFUN_2L.cpp

--- Code v1.1/CFUN_2L.cpp
+++ Code v1.1/CFUN_2L.cpp
@@ -2,7 +2,7 @@
 // Zhangyi He, Xiaoyang Dai, Mark Beaumont and Feng Yu
 
 // version 1.1
-// Two-loucs case (N/A is allowed)
+// Two linked loci with missing genotypes
 
 // C functions
 
@@ -417,7 +417,7 @@ List runBPF_arma(const double& sel_cof_A, const double& dom_par_A, const double&
 /*************************/
 
 
-/********* PMMH **********/
+/********** PMMH **********/
 // Calculate the log-likelihood using the bootstrap particle filter
 // [[Rcpp::export]]
 double calculateLogLikelihood_arma(const double& sel_cof_A, const double& dom_par_A, const double& sel_cof_B, const double& dom_par_B, const double& rec_rat, const int& pop_siz, const arma::irowvec& smp_gen, const arma::irowvec& smp_siz, const arma::imat& smp_cnt, const arma::field<arma::imat>& ptl_ale_cnt, const arma::field<arma::imat>& ptl_hap_cnt, const double& phi, const arma::uword& ptn_num, const arma::uword& pcl_num) {
